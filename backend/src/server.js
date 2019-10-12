0a67008a997b5ab444b388a75515bbf6652241f9
Filipe Cavalcante <filipencavalcante@gmail.com>
2019-10-11 22:01:39 -0300

    I have done the initial part of the backend with the routes and server right configurated and the Session model and Session controller done

---
+++ backend/src/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes');
+
+const app = express();
+
+mongoose.connect('mongodb+srv://omnistack:omnistack@filipeserver-8wfhq.mongodb.net/aircnc?retryWrites=true&w=majority', 
+    {useNewUrlParser:true,
+    useUnifiedTopology: true});
+
+app.use(express.json()); // Deve vir antes do use(routes). Só o que vem depois dessa inha entende instruções JSON
+app.use(routes);
+
+app.listen('3333');
