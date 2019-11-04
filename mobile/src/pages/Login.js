efd4b493adf870b33ac7d35f596ccd39bfd060bc
Filipe Cavalcante <filipencavalcante@gmail.com>
2019-11-03 22:48:16 -0300

    Duing the routing part of the mobile system

--- mobile/src/pages/Login.js
+++ mobile/src/pages/Login.js
@@ -1,10 +1,114 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState, useEffect } from 'react';
 
-export default function Login(){
+import { StyleSheet, AsyncStorage , View, Image, Text, TextInput, TouchableOpacity, KeyboardAvoidingView } from 'react-native';
+
+import api from "../services/api";
+import logo from '../assets/logo.png';
+
+export default function Login({ navigation }){
+    const [email, setEmail] = useState('');
+    const [techs, setTechs] = useState('');
+
+    useEffect(() => {
+        AsyncStorage.getItem('user').then(user => {
+            if (user){
+                navigation.navigate('List');
+            }
+        })
+    },
+    []);
+
+    async function handleSubmit(){
+        const response = await api.post('/sessions', {
+            email,
+        });
+        
+        const { _id } = response.data;
+
+        await AsyncStorage.setItem('user', _id);
+        await AsyncStorage.setItem('techs', techs);
+
+        navigation.navigate('List');
+    }
+
     return( 
-    <View>
-        <Text>Login</Text>
-    </View>
+    <KeyboardAvoidingView behavior='padding' style={styles.container}>
+        <Image source={logo} />
+        
+        <View style={styles.form}>
+            <Text style={styles.label}> SEU E-MAIL *</Text>
+            <TextInput
+                style={styles.input}
+                placeholder="Seu melhor e-mail"
+                placeholderTextColor='#999'
+                keyboardType='email-address'
+                autoCapitalize='none'
+                autoCorrect={false}
+                value={email}
+                onChangeText = {text => setEmail(text)}
+            />
+
+            <Text style={styles.label}> TECNOLOGIAS *</Text>
+            <TextInput
+                style={styles.input}
+                placeholder="Tecnologias de interesse"
+                placeholderTextColor='#999'
+                autoCapitalize='words'
+                autoCorrect={false}
+                value= {techs}
+                onChangeText={setTechs}
+            /> 
+
+            <TouchableOpacity onPress={handleSubmit} style={styles.button}>
+                <Text style={styles.buttonText}> Encontrar spots</Text>
+            </TouchableOpacity>
+        </View>
+    </KeyboardAvoidingView>
     );
 }
+
+const styles = StyleSheet.create({
+    container : {
+        flex:1,
+        alignItems: 'center', //Centraliza horizontalmente
+        justifyContent: 'center', //Centraliza verticalmente
+    },
+
+    form: {
+        alignSelf: 'stretch',
+        paddingHorizontal: 30,
+        marginTop: 30,
+    },
+
+    label : {
+        fontWeight:'bold',
+        color: '#444',
+        marginBottom: 8,
+    },
+
+    input : {
+        borderWidth:1,
+        borderColor: '#DDD',
+        paddingHorizontal: 20,
+        fontSize: 16,
+        color: '#444',
+        height: 44,
+        marginBottom: 20,
+        borderRadius: 4,
+    },
+
+    button: {
+        height:42,
+        backgroundColor: '#f05a5b',
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: 4,
+    },
+
+    buttonText:{
+        color: '#fff',
+        fontSize: 16,
+        fontWeight: 'bold',
+    }
+
+});
